07176ccd41c6155e934c49452aa698172ae4af3d
dongmingjuan <https://github.com/dongmingjuan>
2017-12-08 10:56:03 +0800

    add the routes for sites in paginate

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  # 后台权限管理
   namespace :admin do
     resources :sites
-  end 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
   root "welcome#index"
 end
