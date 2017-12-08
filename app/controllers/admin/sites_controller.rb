1842fe16197e6e4fda8199c5d75bb281ef3e63e9
dongmingjuan <https://github.com/dongmingjuan>
2017-12-08 16:12:00 +0800

    build site index&eidt page

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -1,7 +1,31 @@
 class Admin::SitesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_site, only: [:edit, :update]
   layout "admin"
+
   def index
     @sites = current_user.sites
+    @sites = @sites.page params[:page]
+  end
+
+  def edit
+  end
+
+  def update
+    if @site.update(site_params)
+      redirect_to admin_sites_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_site
+    @site = Site.find(params[:id])
+  end
+
+  def site_params
+    params.require(:site).permit(:site_name, :site_url)
   end
 end
