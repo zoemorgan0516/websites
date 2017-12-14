3332070d7c091b5b458521833b5d4e3de13852d1
dongmingjuan <https://github.com/dongmingjuan>
2017-12-14 15:09:51 +0800

    justify the slider_picture in page

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -2,7 +2,7 @@ class Admin::SitesController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
   before_action :set_site, only: [:edit, :update]
-  layout "admin"
+
 
   def index
     @site = current_user.site
