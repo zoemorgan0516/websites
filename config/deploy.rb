eaed0e084e8675985ebbfc9a8feeb526fe9159fc
dongmingjuan <https://github.com/dongmingjuan>
2017-12-15 19:00:11 +0800

    use command bundle update

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,7 +64,6 @@ task :deploy do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
