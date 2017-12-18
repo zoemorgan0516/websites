487266e96e69be74b086386b764a8b71cf1f7997
dongmingjuan <https://github.com/dongmingjuan>
2017-12-18 17:58:33 +0800

    add advantages controller & views

--- app/models/advantage.rb
+++ app/models/advantage.rb
@@ -3,10 +3,12 @@ class Advantage
   include Mongoid::Timestamps
   # 图片上的内容
   field :content,              type: String
+  # 点击图片跳转地址
   field :url,                  type: String
   # 图片
   field :avatar, type: String
   mount_uploader :avatar,        AvatarUploader
+
   # 关联关系
   belongs_to :site
 end
