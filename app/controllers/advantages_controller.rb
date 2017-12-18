16124fc9f0c20bc4dfbca88fcc44bf872e04991a
dongmingjuan <https://github.com/dongmingjuan>
2017-12-18 18:38:22 +0800

    add advantage_page

--- app/controllers/advantages_controller.rb
+++ app/controllers/advantages_controller.rb
@@ -1,7 +1,7 @@
 class AdvantagesController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
-  layout "admin"
+
 
   def index
     @site = current_user.site
