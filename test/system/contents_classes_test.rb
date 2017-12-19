a0b70efda04f4a5245a452cd6eff2b94047c582c
guoyuhui <aliceguo2016reborn@gmail.com>
2017-12-19 19:10:52 +0800

    link different content list to different path on show's page

---
+++ test/system/contents_classes_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class Admin::ContentsClassesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit admin_contents_classes_url
+  #
+  #   assert_selector "h1", text: "Admin::ContentsClass"
+  # end
+end
