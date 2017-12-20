36a5dbaf7318ff281fd7e82e8b145405bbe934f0
dongmingjuan <https://github.com/dongmingjuan>
2017-12-20 11:11:09 +0800

    add show.html.erb in app/views/articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,44 +1,12 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!
-  load_and_authorize_resource
   before_action :set_article, only: [:edit, :update, :show]
-  layout "admin"
+  layout 'admin'
 
   def index
     @articles = current_user.articles.page params[:page]
   end
 
-  def new
-    @article = Article.new
-  end
-
-  def edit
-  end
-
   def show
-  end
-
-  def create
-    @article = Article.new(article_params)
-    @article.user = current_user
-    if @article.save
-       redirect_to articles_path
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @article.update(article_params)
-       redirect_to articles_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @article.destroy
-    redirect_to articles_path
   end
 
   private
