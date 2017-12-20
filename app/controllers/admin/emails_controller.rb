701c8ca73fc8a007f0b935359f436c78d4b960f6
dongmingjuan <https://github.com/dongmingjuan>
2017-12-20 11:54:42 +0800

    add email model & controller

---
+++ app/controllers/admin/emails_controller.rb
@@ -0,0 +1,2 @@
+class Admin::EmailsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 # 后台网站板式组成部分的路径
   namespace :admin do
     resources :sites
+    resources :emails
     resources :url_addresses
     resources :footers
     resources :categories
@@ -27,6 +28,7 @@ Rails.application.routes.draw do
   end
 # 前台网站展示路径
   resources :sites
+  resources :emails
   resources :contents
   resources :footers
 
