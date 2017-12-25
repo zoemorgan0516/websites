9b1b3bfe6fb804ccb275f803900abed74a5834f8
liandu02 <liandu02@liandu01deMac-Pro.local>
2017-12-25 17:02:09 +0800

    frontier modification

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,7 @@
 //
 //= require rails-ujs
 //= require_tree .
+$(document).ready(function(){
+   var width = $(window).width();  //getting windows height
+   jQuery('#myCarousel').css('width',width+'px');   //and setting height of carousel
+});
