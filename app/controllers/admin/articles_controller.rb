6955935df30ebd300d0120936a61d4607923eaf0
dongmingjuan <https://github.com/dongmingjuan>
2017-12-26 17:38:22 +0800

    change the controller of admin::article_controller

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,30 +1,22 @@
-class Admin::ArticlesController < ApplicationController
-  before_action :authenticate_user!
-  load_and_authorize_resource
+class Admin::ArticlesController < BaseController
   before_action :set_article, only: [:edit, :update, :show]
-  layout "admin"
 
   def index
-    @site = current_user.site
     @articles = @site.articles.page params[:page]
   end
 
   def new
-    @site = current_user.site
     @article = @site.articles.new
   end
 
   def edit
-    @site = current_user.site
   end
 
   def show
-    @site = current_user.site
     @comments = @article.comments
   end
 
   def create
-    @site = current_user.site
     @article = @site.articles.new(article_params)
     if @article.save
        redirect_to admin_articles_path
@@ -34,7 +26,6 @@ class Admin::ArticlesController < ApplicationController
   end
 
   def update
-    @site = current_user.site
     if @article.update(article_params)
        redirect_to admin_articles_path
     else
@@ -43,7 +34,6 @@ class Admin::ArticlesController < ApplicationController
   end
 
   def destroy
-    @site = current_user.site
     @article.destroy
     redirect_to admin_articles_path
   end
