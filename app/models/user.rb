1cfb7908807ee36a2a34fe0f9915a8d8eb67f129
liandu02 <liandu02@liandu01deMac-Pro.local>
2017-12-27 17:34:44 +0800

    restore User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User
   paginates_per 5
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   ## Database authenticatable
